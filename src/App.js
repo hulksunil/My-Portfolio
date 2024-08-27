8f15d1aead914b2945bc74a8657fddbb72a1a9d5
hulksunil <hulksunil@hotmail.com>
2024-08-26 20:27:33 -0400

    started adding projects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,14 +5,38 @@ import Intro from "./components/Intro";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
+import bouncingBall from "./assets/img/bouncing-ball.gif";
+import carsRUs from "./assets/img/cars-r-us.gif";
+import Contact from "./components/Contact";
+
+const projects = [
+  {
+    title: "Bouncing Ball",
+    description:
+      "A ball that bounces. Also includes a little game where you have to try to hit all the targets as fast as possible",
+    tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+    link: "https://gitlab.com/hulksunil/bouncing-ball",
+    img: bouncingBall,
+  },
+  {
+    title: "Car Rental Application",
+    description:
+      "This car rental app connects users with vehicles for short-term rentals, ranging from a few hours to a few weeks, serving as the interface between customers and the rental company.",
+    tags: ["MongoDB", "Express", "ReactJS", "Node.js"],
+    link: "https://github.com/username/project-two",
+    img: carsRUs,
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Intro />
-      <Projects />
+      <Projects projects={projects} />
 
       <Skills />
+      <Contact />
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,29 @@
-import { Container } from "react-bootstrap";
+import { Badge, Col, Container } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
-const Projects = () => {
+function ProjectCard({ title, description, tags, link, img }) {
+  return (
+    <Card className="projectCard">
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Img variant="top" src={img} />
+        <Card.Text>{description}</Card.Text>
+        <div className="projectKeywords">
+          {tags.map((tag) => (
+            <Badge key={tag} variant="secondary" className="mr-2">
+              {tag}
+            </Badge>
+          ))}
+        </div>
+        <Button variant="primary" href={link} target="_blank">
+          View Project
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+const Projects = ({ projects }) => {
   return (
     <Container id="projects" className="projects">
       <h1>Projects</h1>
@@ -8,6 +31,14 @@ const Projects = () => {
         I love working on projects! Here are some of the projects that I have
         worked on:
       </p>
+
+      <Container className="container-fluid d-flex justify-content-between">
+        {projects.map((project) => (
+          <Col xs={12} md={6} lg={4} key={project.title}>
+            <ProjectCard {...project} />
+          </Col>
+        ))}
+      </Container>
     </Container>
   );
 };
