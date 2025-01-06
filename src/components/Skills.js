3ce8483a2c23225d553e0bbbc4f375f064ebbb00
hulksunil <hulksunil@hotmail.com>
2025-01-06 02:01:43 -0500

    tried to make it flash in but I just put a simple fade in transition instead. will try this another time

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import {
   FaCode,
@@ -6,8 +7,42 @@ import {
   FaLanguage,
   FaTools,
 } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
+
+
+
+const SkillCard = ({ skillIcons, category, skillList }) => {
+  const { ref: skillRef, inView: skillIsVisible } = useInView();
+  const [hasBeenVisible, setHasBeenVisible] = useState(false);
+
+  // This is so that the fade-in animation only happens once
+  useEffect(() => {
+    if (skillIsVisible) {
+      setHasBeenVisible(true);
+    }
+  }, [skillIsVisible]);
+
+
+  return <Card ref={skillRef} className={`skillCard flash-in-section text-light h-100 ${hasBeenVisible ? "is-visible" : ""}`}>
+    <Card.Body>
+      <Card.Title className="d-flex align-items-center">
+        {skillIcons[category]}
+        <span className="ms-2">{category}</span>
+      </Card.Title>
+      <ul className="list-unstyled">
+        {skillList.map((skill) => (
+          <li key={skill}>{skill}</li>
+        ))}
+      </ul>
+    </Card.Body>
+  </Card>;
+}
+
+
 
 const Skills = () => {
+
+
   const skills = {
     Programming: [
       "Java",
@@ -43,30 +78,22 @@ const Skills = () => {
     Languages: <FaLanguage className="hoverIcon" />,
   };
 
+
+
   return (
     <Container className="py-5 mainHeader" id="skills">
       <h1 className="text-center text-light mb-4">Skills</h1>
       <Row>
         {Object.entries(skills).map(([category, skillList]) => (
           <Col key={category} md={6} lg={4} className="mb-4">
-            <Card className="skillCard text-light h-100">
-              <Card.Body>
-                <Card.Title className="d-flex align-items-center">
-                  {skillIcons[category]}
-                  <span className="ms-2">{category}</span>
-                </Card.Title>
-                <ul className="list-unstyled">
-                  {skillList.map((skill) => (
-                    <li key={skill}>{skill}</li>
-                  ))}
-                </ul>
-              </Card.Body>
-            </Card>
+            <SkillCard skillIcons={skillIcons} category={category} skillList={skillList} />
           </Col>
         ))}
       </Row>
-    </Container>
+    </Container >
   );
 };
 
 export default Skills;
+
+
