7062bc228b56c57723bb23c4aefdcb371697a8e8
Sunil Kublalsingh <hulksunil@hotmail.com>
2026-02-25 03:28:42 -0500

    updated fonts

--- src/components/utils/HeadingTitle.js
+++ src/components/utils/HeadingTitle.js
@@ -1,7 +1,7 @@
 const HeadingTitle = ({ title, description }) => {
     return (
         <div className="text-center mb-20 mainHeading">
-            <h2 className="text-4xl font-display font-bold mb-4 pt-4">{title}</h2>
+            <h2 className="text-4xl md:text-5xl font-display font-extrabold tracking-tight mb-4 pt-4">{title}</h2>
             {description ? (
                 <p className="text-slate-500 max-w-2xl mx-auto text-lg">{description}</p>
             ) : (
